1eb8dbf47e460dc44c22928be23a61a4264b4c10
GanpatHada <hadaganpat42@gmail.com>
2023-07-06 19:42:39 +0530

    fix : alignment in profile

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import './Home.css'
-import LeftSideBar from '../../components/left-side-bar/LeftSideBar'
-import RightSideBar from '../../components/right-side-bar/RightSideBar'
-import BottomNavbar from '../../components/bottom-navbar/BottomNavbar'
-import Profile from '../profile/Profile'
+import React from "react";
+import "./Home.css";
+import LeftSideBar from "../../components/left-side-bar/LeftSideBar";
+import RightSideBar from "../../components/right-side-bar/RightSideBar";
+import BottomNavbar from "../../components/bottom-navbar/BottomNavbar";
+import Profile from "../profile/Profile";
 const Home = () => {
   return (
-    <div id='home-page'>
-        <LeftSideBar/>
-        <main>
-         <Profile/>
-        </main>
-        <RightSideBar/>
-        <BottomNavbar/>
+    <div id="home-page">
+      <LeftSideBar />
+      <main>
+        <div id="main-content">
+          <Profile />
+        </div>
+      </main>
+      <RightSideBar />
+      {/* <BottomNavbar/> */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/pages/profile/components/profile-nav/ProfileNav.jsx
+++ src/pages/profile/components/profile-nav/ProfileNav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './ProfileNav.css'
-import { BsFillGrid3X3GapFill,BsFillBookmarksFill } from "react-icons/bs";
+import { BsGrid3X3,BsBookmarks } from "react-icons/bs";
 import { AiOutlineHeart } from 'react-icons/ai';
 const ProfileNav = () => {
   return (
@@ -8,9 +8,9 @@ const ProfileNav = () => {
        
         <ul className='all-centered'>
         <div id="active-nav"></div>
-            <li className='all-centered'> <span><BsFillGrid3X3GapFill/></span> Posts</li>
+            <li className='all-centered'> <span><BsGrid3X3/></span> Posts</li>
             <li className='all-centered'> <span> <AiOutlineHeart /></span>Likes</li>
-            <li className='all-centered'> <span> <BsFillBookmarksFill/></span>Saved</li>
+            <li className='all-centered'> <span> <BsBookmarks/></span>Saved</li>
         </ul>
       </nav>
   )
